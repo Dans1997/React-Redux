d3ea8260ae217faef3d27e26d307991dc31ba68a
Danillo Neves Souza <danillo.neves97@gmail.com>
2020-02-28 11:53:03 -0300

    Implemented Redux Thunk

---
+++ src/actions/index.js
@@ -0,0 +1,11 @@
+import jsonPlaceholder from '../apis/jsonPlaceholder'
+
+// Using Redux Thunk to Return a Function to deal with Async Stuff
+export const fetchPosts = () => async dispatch => 
+{
+    const response = await jsonPlaceholder.get('/posts');
+
+    dispatch({
+        type: 'FETCH_POSTS', payload: response
+    })
+}
--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
 
 import App from './components/App';
+import Reducers from './reducers'
 
-ReactDOM.render(<App />, document.querySelector('#root'));
+const store = createStore(Reducers, applyMiddleware(thunk));
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>
+    , document.querySelector('#root'));
 
