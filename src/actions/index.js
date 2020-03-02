fe1af2e13d42d52fb8e724c4d1d1089383b9b831
Danillo Neves Souza <danillo.neves97@gmail.com>
2020-03-02 09:35:00 -0300

    Another Overfetching Solution

--- src/actions/index.js
+++ src/actions/index.js
@@ -12,7 +12,6 @@ export const fetchPosts = () => async dispatch =>
     })
 }
 
-// Overfetching Solution #1 -> Reusing Action Creators fetchPostsAndUsers()
 export const fetchUser = (id) => async dispatch => 
 {
     const response = await jsonPlaceholder.get(`/users/${id}`);
@@ -21,6 +20,20 @@ export const fetchUser = (id) => async dispatch =>
         type: 'FETCH_USER',
         payload: response.data
     })
+}
+
+// Overfetching Solution #1 -> Reusing Action Creators fetchPostsAndUsers()
+export const fetchPostsAndUsers = () => async (dispatch, getState) => {
+    await dispatch(fetchPosts());
+    const userIds = _.uniq(_.map(getState().posts, 'userId'));
+    userIds.forEach(id => dispatch(fetchUser(id)));
+
+    /* BONUS: LODASH CHAIN EXAMPLE */
+    _.chain(getState().posts)
+        .map('userId')
+        .uniq()
+        .forEach(id => dispatch(fetchUser(id)))
+        .value()
 }
 
 // Overfetching Solution #2 -> MEMOIZE
